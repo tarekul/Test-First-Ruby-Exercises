e19082eadf2852876292ff78482370fe3f026c2c
Tarekul Islam <bayridge4th@gmail.com>
2017-05-09 18:11:06 -0400

    Redid xml document using missing_method

--- lib/13_xml_document.rb
+++ lib/13_xml_document.rb
@@ -1,74 +1,28 @@
 class XmlDocument
   # TODO: your code goes here!
-  def initialize(to_indent = false)
-  	@to_indent = to_indent
-  	@indent = 0
+  def initialize(indent = false)
+  	@indent = indent
+  	@indent_level = 0
   end
-  
-  def hello(input = nil)
-  	hold = ""
-  	if input
-  		input.each_value do |y|
-  			hold = 	%Q(<hello name="#{y}"/>)
-  		end
-	else
-  		hold = "<hello/>"
-  		if block_given?
-  			if @to_indent
-  				hold = "<hello>\n" + yield + "</hello>\n"
-  			else
-  				hold = "<hello>" + yield + "</hello>"	
-  			end
-  		end
+
+  def method_missing(method_symbol, *args)
+  	xml = ""
+  	xml += "<" + method_symbol.to_s
+  	args[0] != nil ? attributes = args[0] : attributes = {}
+  	attributes.each do |k,v|
+		xml += %Q( #{k}="#{v}")
   	end
-  	return hold 
-  end
 
-  def send(tag_name = nil)
-  	return "<#{tag_name}/>"
-  end
-
-  def goodbye
-  	hold =  "<goodbye/>"
-  	space = " "
-  	@indent += 2
-  	space *= @indent
   	if block_given?
-  		if @to_indent
-  			hold = space + "<goodbye>\n" + yield + space + "</goodbye>\n"
+  		@indent_level += 2
+  		space = " " * @indent_level
+  		if @indent
+  			xml += ">\n" + space + yield 
   		else
-  			hold = "<goodbye>" + yield + "</goodbye>"
+  			xml + ">" + yield + "</" + method_symbol.to_s + ">"
   		end
-  	end
-  	return hold
-  end
-
-  def come_back
-  	if @to_indent
-  		space = " "
-  		@indent += 2
-  		space = space * @indent 
-  		hold = space + "<come_back>\n" + yield + space + "</come_back>\n"
   	else
-  		hold = "<come_back>" + yield + "</come_back>"
-  	end
-  end
-
-  def ok_fine(input)
-  	key = ""
-  	value = ""
-  	input.each do |k,v|
-  		key = k
-  		value = v 
-  	end
-
-  	if @to_indent
-  		space = " "
-  		@indent += 2
-  		space *= @indent
-  		return %Q(#{space}<ok_fine #{key.to_s}="#{value}"/>\n)
-  	else
-  		return %Q(<ok_fine #{key.to_s}="#{value}"/>)
+  		return xml + "/>"
   	end
   end
 end
